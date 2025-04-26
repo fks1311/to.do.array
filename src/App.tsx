cc348c9faa703bb9c2ae0823b12497456dc7a5cb
ranyoung <fks1311@gmail.com>
2025-04-26 19:32:30 +0900

    feat: Header 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import styled from "styled-components";
+import { Header } from "./components/Head";
+import { Nav } from "./components/Nav";
+import { Timer } from "./components/Timer/Timer";
+import { Todo } from "./components/Todo/Todo";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Layout>
+      <Header />
+      <Main>
+        <Nav />
+        <Timer />
+        <Todo />
+      </Main>
+    </Layout>
+  );
 }
 
 export default App;
+
+const Layout = styled.div`
+  height: 100%;
+`;
+const Main = styled.main`
+  height: 84%;
+  display: flex;
+`;
---
+++ src/components/Head.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import Logo from "../assets/icon.png";
+import { JSX } from "react";
+
+export const Header = (): JSX.Element => {
+  return (
+    <Head>
+      <img src={Logo} />
+      <p>to•do•focus</p>
+    </Head>
+  );
+};
+
+const Head = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  // padding: 1rem;
+  height: 15%;
+  border-bottom: 1px solid ${({ theme: { darkmode } }) => darkmode.divider};
+  img {
+    height: 4rem;
+    border-radius: 50%;
+    margin-right: 30px;
+  }
+  p {
+    font-size: 3rem;
+    color: ${({ theme: { darkmode } }) => darkmode.txt_primary};
+  }
+`;
